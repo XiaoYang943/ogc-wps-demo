947043758953056bdfd070413eadbf05bce38e8b
heyiyang <heyiyang@linespace.cn>
2025-05-14 16:03:28 +0800

    init: 初始化

---
+++ src/main/java/org/example/wps/OgcWpsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.wps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgcWpsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgcWpsDemoApplication.class, args);
+    }
+
+}
